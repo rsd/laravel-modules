1a910de2bdf736727eff7a4f11cd2f423a543e2e
Raul Dias <raul@dias.com.br>
2025-09-22 14:47:28 -0300

    feat: add ConfigMergerTrait for merging configuration defaults

---
+++ src/Traits/ConfigMergerTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Nwidart\Modules\Traits;
+
+use Illuminate\Contracts\Foundation\CachesConfiguration;
+
+trait ConfigMergerTrait
+{
+    /**
+     * Merge configuration defaults from a file, allowing existing config to take precedence.
+     *
+     * @param  string  $path
+     * @param  string  $key
+     * @param  bool  $top_level_precedence  Whether to use top level precedence or not
+     * @param  bool  $deep  Whether to use deep merging (array_replace_recursive) or shallow merging (array_merge)
+     * @return void
+     */
+    protected function mergeConfigDefaultsFrom($path, $key, $top_level_precedence = true, $deep = true)
+    {
+        if (! ($this->app instanceof CachesConfiguration && $this->app->configurationIsCached())) {
+            $config = $this->app->make('config');
+            $existing = $config->get($key, []);
+            $new = require $path;
+
+            if ($deep) {
+                if ($top_level_precedence) {
+                    $config->set($key, array_replace_recursive($new, $existing));
+                } else {
+                    $config->set($key, array_replace_recursive($existing, $new));
+                }
+            } else {
+                if ($top_level_precedence) {
+                    $config->set($key, array_merge($new, $existing));
+                } else {
+                    $config->set($key, array_merge($existing, $new));
+                }
+            }
+        }
+    }
+}
